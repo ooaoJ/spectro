0695d4ceb9a0b37430d39c1187bfc05fb1fa1413
Joaoo <146839436+ooaoJ@users.noreply.github.com>
2024-10-03 15:45:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let cardMain = document.getElementById('card');
+
+function openAbout(id){
+    let cardContent = document.getElementById('card'+id)
+    switch(id){
+        case '1':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '2':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '3':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '4':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '5':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '6':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '7':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        case '8':
+            cardMain.style.display = "flex";
+            cardContent.style.display = "flex";
+            break;
+        default:
+            alert("Invalid Card ID");
+            break;
+    }
+}
+function closeCard(buttonElement){
+    let cardContent = buttonElement.parentElement;
+    cardContent.style.display = "none";
+    cardMain.style.display = "none";
+}
+
+function closeAllCards(){
+    let cardContents = document.getElementsByClassName('card-content');
+    for(let i=0; i<cardContents.length; i++){
+        cardContents[i].style.display = "none";
+    }
+    cardMain.style.display = "none";
+
+}
